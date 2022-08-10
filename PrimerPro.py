5a70ec9907de238ddc2f40b607fd3c31bb830feb
cdisoftware <juanbmorenob@hotmail.com>
2022-08-10 18:25:15 -0500

    Initial commit

---
+++ PrimerPro.py
@@ -0,0 +1,26 @@
+'''
+Imprimir valores con funcion Print()
+'''
+#print("Juan tu papa ´prueba´")
+'''
+Caracteres especiales
+'''
+'''
+Comillas dobles
+'''
+#print("Prueba \"Juan\"")
+'''
+Salto de linea
+'''
+#print("Prueba \nJuan")
+'''
+Tabulacion
+'''
+#print("Prueba \tJuan")
+'''
+Input
+'''
+#print("Tu nombre es: " + input("Dime tu nombre: "))
+
+
+
