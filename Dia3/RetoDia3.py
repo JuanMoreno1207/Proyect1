306bbb7b9b8863f1bb1e581232637f5a9f7f457f
DIGITEACHBOX29041209 <lina.fernanda.rc@gmail.com>
2022-08-16 16:09:20 -0500

    Desarrollo reto 3

---
+++ Dia3/RetoDia3.py
@@ -0,0 +1,24 @@
+mi_texto = str(input('Escribe tu texto, por favor : ')).lower()
+mis_letras = list(input('Escribe 3 letras, por favor : ').lower())
+
+cantidad1 = mi_texto.count(mis_letras[0])
+cantidad2 = mi_texto.count(mis_letras[1])
+cantidad3 = mi_texto.count(mis_letras[2])
+cantidadP = mi_texto.split()
+fragmento1 = mi_texto[0]
+fragmento2 = mi_texto[-1]
+Textoalrev = list(mi_texto)
+Textoalrev.reverse()
+TextoStr = "".join(Textoalrev)
+diccionario = dict
+Existe = ("python" in mi_texto)
+diccionario = {'Existe': Existe}
+
+print(f"La letra {mis_letras[0]} aparece {cantidad1} veces en el texto")
+print(f"La letra {mis_letras[1]} aparece {cantidad2} veces en el texto")
+print(f"La letra {mis_letras[2]} aparece {cantidad3} veces en el texto")
+print(f"El texto tiene {len(cantidadP)} palabras")
+print(f"La primera letra del texto es {fragmento1} y la ultima es {fragmento2}")
+print(f"El texto al reves da como resultado : {TextoStr}")
+print(f"La palabra python existe en el texto? : {diccionario}")
+
