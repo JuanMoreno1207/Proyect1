4194c6f194ad11bca57a2414bb525bb916bf9fdd
DIGITEACHBOX29041209 <lina.fernanda.rc@gmail.com>
2022-08-16 16:44:55 -0500

    Desarrollo reto 3

--- Dia3/RetoDia3.py
+++ Dia3/RetoDia3.py
@@ -10,15 +10,13 @@ fragmento2 = mi_texto[-1]
 Textoalrev = list(mi_texto)
 Textoalrev.reverse()
 TextoStr = "".join(Textoalrev)
-diccionario = dict
 Existe = ("python" in mi_texto)
-diccionario = {'Existe': Existe}
-
+diccionario = {True: 'Si', False: 'No'}
 print(f"La letra {mis_letras[0]} aparece {cantidad1} veces en el texto")
 print(f"La letra {mis_letras[1]} aparece {cantidad2} veces en el texto")
 print(f"La letra {mis_letras[2]} aparece {cantidad3} veces en el texto")
 print(f"El texto tiene {len(cantidadP)} palabras")
 print(f"La primera letra del texto es {fragmento1} y la ultima es {fragmento2}")
 print(f"El texto al reves da como resultado : {TextoStr}")
-print(f"La palabra python existe en el texto? : {diccionario}")
+print(f"La palabra python {diccionario[Existe]} existe en el texto")
 
