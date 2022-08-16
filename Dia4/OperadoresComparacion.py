dbb93bfee5874d603406077fb803503db44e37ec
DIGITEACHBOX29041209 <lina.fernanda.rc@gmail.com>
2022-08-16 17:04:45 -0500

    Desarrollo reto 3

---
+++ Dia4/OperadoresComparacion.py
@@ -0,0 +1,3 @@
+mi_bool = 100 == 100
+mi_bool = 99 != 100
+print(mi_bool)
---
+++ Dia4/OperadoresLogicos.py
@@ -0,0 +1,9 @@
+mi_bool = (4 < 5) and (5 == 5)
+print(mi_bool)
+mi_bool = (5 < 5) or (6 == 5)
+print(mi_bool)
+texto = 'Juan Moreno'
+mi_bool = ('Juan' in texto) and ('Moreno' in texto)
+print(mi_bool)
+mi_bool = not 'a' == 'a'
+print(mi_bool)
